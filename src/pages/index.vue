50dc27764187769b5f6fd0f3a775bd02dc49906f
Eadweard <804875728@qq.com>
2022-07-13 22:59:44 +0800

    change

---
+++ src/pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const name = $ref('')
+
+const router = useRouter()
+const go = () => {
+  if (name)
+    router.push(`/hi/${encodeURIComponent(name)}`)
+}
+</script>
+
+<template>
+  <div>
+    <div>
+      <button
+        class="m-3 text-sm btn"
+        :disabled="!name"
+        @click="go"
+      >
+        Go
+      </button>
+    </div>
+  </div>
+</template>
