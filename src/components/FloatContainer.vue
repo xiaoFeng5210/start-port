6783b84b0a40d5fb2b9054778e7fcc31b926119e
Eadweard <804875728@qq.com>
2022-07-18 23:30:22 +0800

    传递style

--- src/components/FloatContainer.vue
+++ src/components/FloatContainer.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
-import { metadata } from '~/composables/floating'
+import { metadata, proxyEl } from '~/composables/floating'
+
+const rect = ref<DOMRect>({
+  top: 0,
+  left: 0,
+  width: 0,
+  height: 0,
+  bottom: 0,
+  right: 0,
+} as DOMRect)
+watch(proxyEl, (el) => {
+  rect.value = el?.getBoundingClientRect() ?? rect.value
+}, { immediate: true })
+const style = computed(() => {
+  const { top, left, width, height } = rect.value
+  return {
+    top: `${top}px`,
+    left: `${left}px`,
+    width: `${width}px`,
+    height: `${height}px`,
+  }
+})
 </script>
 
 <template>
-  <div fixed>
+  <div :style="style">
     <slot v-bind="metadata.attrs" />
   </div>
 </template>
