dda8819d35cc0015d73af8a7923bd06fd85e929b
Eadweard <804875728@qq.com>
2022-08-24 21:44:28 +0800

    函数组件

--- src/composables/floating.ts
+++ src/composables/floating.ts
@@ -1,4 +1,5 @@
-import type { Component } from "vue";
+import type { Component, StyleValue } from "vue";
+import { h } from "vue";
 
 export const metadata = reactive<any>({
   props: {},
@@ -13,9 +14,70 @@ export function createFloating<T extends Component>(component: T) {
     attrs: {},
   });
 
-  const proxyEl = ref<HTMLElement | null>()
+  const proxyEl = ref<HTMLElement | null>();
 
-  const container = defineComponent({})
+  const container = defineComponent({
+    setup() {
+      const rect = ref<DOMRect | undefined>({
+        top: 0,
+        left: 0,
+        width: 0,
+        height: 0,
+        bottom: 0,
+        right: 0,
+      } as DOMRect);
+      watch(
+        proxyEl,
+        (el) => {
+          rect.value = el?.getBoundingClientRect() ?? rect.value;
+        },
+        { immediate: true }
+      );
+      const style = computed((): StyleValue => {
+        return {
+          transition: "all .5s ease-in-out",
+          position: "fixed",
+          top: `${rect.value?.top ?? 0}px`,
+          left: `${rect.value?.left ?? 0}px`,
+        };
+      });
+      function update() {
+        rect.value = proxyEl.value?.getBoundingClientRect();
+      }
 
-  const proxy = defineComponent({})
+      useMutationObserver(proxyEl, update, {
+        childList: true,
+        subtree: true,
+        attributes: true,
+        characterData: true,
+      });
+
+      useEventListener("resize", update);
+      watchEffect(update);
+
+      return () =>
+        h("div", { style: style.value }, [h(component, metadata.attrs)]);
+    },
+  });
+
+  const proxy = defineComponent({
+    setup(props, ctx) {
+      // const props = defineProps<{}>()
+      const attrs = useAttrs()
+      metadata.props = props
+      metadata.attrs = attrs
+      const el = ref<HTMLElement>()
+      onMounted(() => {
+        proxyEl.value = el.value
+      })
+      return () => h('div', { ref: el }, [
+        ctx.slots.default ? h(ctx.slots.default) : null
+      ])
+    },
+  })
+
+  return {
+    container,
+    proxy,
+  };
 }
