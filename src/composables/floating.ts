f9eca31c5e904a116f280299f646a067b07401c6
Eadweard <804875728@qq.com>
2022-08-27 00:30:43 +0800

    fix

--- src/composables/floating.ts
+++ src/composables/floating.ts
@@ -1,18 +1,18 @@
-import type { Component, StyleValue } from "vue"
-import { h } from "vue"
+import type { Component, StyleValue } from 'vue'
+import { h } from 'vue'
 
 export const metadata = reactive<any>({
   props: {},
   attrs: {},
 })
 
-export const proxyEl = ref<HTMLElement | null>();
+export const proxyEl = ref<HTMLElement | null>()
 
 export function createFloating<T extends Component>(component: T) {
   const metadata = reactive<any>({
     props: {},
     attrs: {},
-  });
+  })
 
   const proxyEl = ref<HTMLElement | null>()
 
@@ -63,6 +63,8 @@ export function createFloating<T extends Component>(component: T) {
     setup(props, ctx) {
       const attrs = useAttrs()
       metadata.attrs = attrs
+      metadata.props = props
+      console.log(metadata)
       const el = ref<HTMLElement>()
       onMounted(() => {
         proxyEl.value = el.value
--- src/pages/foo.vue
+++ src/pages/foo.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { Proxy } from '../composables/image'
 let size = $ref(100)
 function enlarge() {
   size += 20
@@ -15,7 +16,7 @@ function enlarge() {
     <p>
       Vue.js is an MIT-licensed open source project with its ongoing development made possible entirely by the support of these awesome backers. If you'd like to join them, please consider sponsor Vue's development
     </p>
-    <FloatProxy :style="{ width: size + 'px', height: size + 'px' }" w-50 h-100 rounded="1/4" />
+    <Proxy :style="{ width: size + 'px', height: size + 'px' }" w-50 h-100 rounded="1/4" />
   </div>
   <!-- <TheImage w-50 h-100 rounded-xl m10 /> -->
 </template>
